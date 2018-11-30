56aef6168ebebf6ed39de97f73e7cad2d60f5e10
Etudiant <email@example.com>
2018-11-30 16:24:09 +0100

    Correction Doctrine Repo

--- src/Service/EventService.php
+++ src/Service/EventService.php
@@ -1,44 +1,24 @@
 <?php
-
 namespace App\Service;
 
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Event;
+
 class EventService {
+    private $om;
 
-    private $events = [
-        [
-            "id"=> 1,
-            "title"=>"Titre 1",
-            "description"=>"nfkjbhvkshhvsdhni",
-            "start_date"=> "2018-11-29 08:00:00",
-            "end_date"=> "2018-11-29 23:00:00"
-        ],
-        [
-            "id"=> 2,
-            "title"=>"Titre 2",
-            "description"=>"ttototoo",
-            "start_date"=> "2018-12-01 08:00:00",
-            "end_date"=> "2018-12-01 23:00:00"
-        ],
-        [
-            "id"=> 3,
-            "title"=>"Titre 3",
-            "description"=>"blablabal",
-            "start_date"=> "2018-11-25 08:00:00",
-            "end_date"=> "2018-11-25 23:00:00"
-        ]
-    ];
+    public function __construct( ObjectManager $om ){
+        $this->om = $om;
+    }
 
-    public function getAll() {
-        return $this->events;
+    public function getAll(){
+        $repo = $this->om->getRepository( Event::class );
+        return $repo->findAll();
     }
 
     public function getOne( $id ) {
-
-        foreach ($this->events as $key => $value) {
-            if ($value['id'] == $id)
-                return $value;
-        }
-        return null;
+        $repo = $this->om->getRepository( Event::class );
+        return $repo->find( $id );
     }
 
 }
